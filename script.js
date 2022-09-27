32e0c702d880b2646e2d02a1b0fcf74c4a495f28
SonSonder <maceomaddox@gmail.com>
2022-09-26 22:39:40 -0400

    I've been working on this for a while, but felt the need to approach completion due to a job opportunity.. a lot has been covered at this juncture

---
+++ script.js
@@ -0,0 +1,23 @@
+const gifSource = document.getElementById("my-gif")
+const dnd = document.getElementById("dnd");
+const header = document.getElementById("header");
+const linkedIn = document.getElementById("linkedIn");
+
+gifSource.onclick = function() {
+    location.href = "https://maceomaddox.github.io/my-GIF/";
+    console.log("clicked...");
+};
+
+dnd.onclick = function() {
+    location.href = "https://maceomaddox.github.io/my-GIF/";
+    console.log("clicked...");
+};
+
+// header.onclick = function() {
+//     console.log("clicked");
+//     location.href="https://maceomaddox.github.io/my-GIF/"
+// }
+
+linkedIn.onclick = function() {
+    location.href = "https://www.linkedin.com/in/maceomaddox/";
+}
